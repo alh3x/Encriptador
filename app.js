0771179200d15a2f1871c1beac301a60b8319dfb
alex <oliver-pinto@outlook.com>
2024-02-05 18:36:16 -0300

    funcion copiar y llamado de funcioes en appjs

--- app.js
+++ app.js
@@ -1,48 +1,70 @@
-const llavesEncriptar = ["e","i","a","o","u"];
-const llavesDesencriptar = ["enter","imes","ai","ober","ufat"]
+const llavesEncriptar = ["e", "i", "a", "o", "u"];
+const llavesDesencriptar = ["enter", "imes", "ai", "ober", "ufat"]
 const textEntrada = document.getElementById('text-entrada');
 const textSalida = document.getElementById('text-salida');
 const botonEncriptar = document.getElementById('encriptar');
 const botonDesencriptar = document.getElementById('desencriptar');
+const botonCopiar = document.getElementById('copiar');
+
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.addEventListener('click',desencriptar);
+
 
 
 function minuscula() {
     let text = textEntrada.value.toLowerCase();
     let regexAcentos = /[áéíóú]/g;
-    let vocales= [['á','a'],['é','e'],['í','i'],['ó','o'],['ú','u']];
+    let vocales = [['á', 'a'], ['é', 'e'], ['í', 'i'], ['ó', 'o'], ['ú', 'u']];
     let regecSimbolos = /[^a-z0-9\s]/g;
-    text=text.replace(regexAcentos,function(valor){
-        for ( let i = 0;i<vocales.length;i++){
-            if(valor === vocales[i][0]){
+    text = text.replace(regexAcentos, function (valor) {
+        for (let i = 0; i < vocales.length; i++) {
+            if (valor === vocales[i][0]) {
                 return vocales[i][1];
             }
         }
-        })
-    text=text.replace(regecSimbolos, "");
-    textEntrada.value=text;
+    })
+    text = text.replace(regecSimbolos, "");
+    textEntrada.value = text;
     console.log(textEntrada.value)
 }
 
-
 function limpiarTexto(elemento) {
-    let valorTexto = elemento.value= "";
+    let valorTexto = elemento.value = "";
 };
 
 function encriptar() {
     let text = textEntrada.value;
-    text = text.replace(/[eiaou]/g, function(valor){
-        for (let i = 0; i<llavesEncriptar.length;i++){
-            if(valor===llavesEncriptar[i]){
+    text = text.replace(/[eiaou]/g, function (valor) {
+        for (let i = 0; i < llavesEncriptar.length; i++) {
+            if (valor === llavesEncriptar[i]) {
                 return llavesDesencriptar[i]
             }
         }
     })
-    return text;
+    textSalida.value = text;
+    limpiarTexto(textEntrada)
 };
-function desencriptar(){
+function desencriptar() {
     let text = textEntrada.value;
-    for(let i = 0; i<llavesDesencriptar.length;i++){
-        text=text.replace(llavesDesencriptar[i], llavesEncriptar[i])
+    for (let i = 0; i < llavesDesencriptar.length; i++) {
+        text = text.replace(llavesDesencriptar[i], llavesEncriptar[i])
     }
-    return text;
+    textSalida.value = text;
+    limpiarTexto(textEntrada)
 }
+
+function copiar() {
+    botonCopiar.addEventListener("click", writeClipboardText);
+    async function writeClipboardText() {
+        try {
+            let text = textSalida.value;
+            await navigator.clipboard.writeText(text);
+            limpiarTexto(textSalida)
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+}
+
+copiar();
