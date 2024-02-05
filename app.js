97cc698f72eed062f3c2e25704a103eff0ce0175
alex <oliver-pinto@outlook.com>
2024-02-05 13:19:16 -0300

    funciones encriptar y desencriptar

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const llavesEncriptar = ["e","i","a","o","u"];
 const llavesDesencriptar = ["enter","imes","ai","ober","ufat"]
 const textEntrada = document.getElementById('text-entrada');
 const textSalida = document.getElementById('text-salida');
+const botonEncriptar = document.getElementById('encriptar');
+const botonDesencriptar = document.getElementById('desencriptar');
+
 
 function minuscula() {
     let text = textEntrada.value.toLowerCase();
@@ -9,7 +12,6 @@ function minuscula() {
     let vocales= [['á','a'],['é','e'],['í','i'],['ó','o'],['ú','u']];
     let regecSimbolos = /[^a-z0-9\s]/g;
     text=text.replace(regexAcentos,function(valor){
-        /* return {'á':'a','é':'e','í':'i','ó':'o','ú':'u'}[valor]; */
         for ( let i = 0;i<vocales.length;i++){
             if(valor === vocales[i][0]){
                 return vocales[i][1];
@@ -19,4 +21,28 @@ function minuscula() {
     text=text.replace(regecSimbolos, "");
     textEntrada.value=text;
     console.log(textEntrada.value)
+}
+
+
+function limpiarTexto(elemento) {
+    let valorTexto = elemento.value= "";
+};
+
+function encriptar() {
+    let text = textEntrada.value;
+    text = text.replace(/[eiaou]/g, function(valor){
+        for (let i = 0; i<llavesEncriptar.length;i++){
+            if(valor===llavesEncriptar[i]){
+                return llavesDesencriptar[i]
+            }
+        }
+    })
+    return text;
+};
+function desencriptar(){
+    let text = textEntrada.value;
+    for(let i = 0; i<llavesDesencriptar.length;i++){
+        text=text.replace(llavesDesencriptar[i], llavesEncriptar[i])
+    }
+    return text;
 }
